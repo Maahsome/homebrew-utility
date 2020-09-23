28f009dbdd6314d91774b8fe76dc9a4a3711b600
Dalton Maahs <dalton.maahs@yourbind.com>
2020-09-23 01:38:52 -0500

    Fix class name in formula

--- Formula/gswm.rb
+++ Formula/gswm.rb
@@ -1,4 +1,4 @@
-class Tn < Formula
+class Gswm < Formula
   desc "gswm: git switch menu. Uses fzf to switch branches in git"
   homepage "https://github.com/maahsome/homebrew-utility"
   url "https://github.com/maahsome/homebrew-utility.git"

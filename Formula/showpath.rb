e3721f4d5a9494a81a56e123dc49a995044d940f
Dalton Maahs <dalton.maahs@yourbind.com>
2020-09-23 01:13:14 -0500

    Use tr rather than sed

--- Formula/showpath.rb
+++ Formula/showpath.rb
@@ -2,7 +2,7 @@ class Showpath < Formula
   desc "Displays the PATH variable such that each directory is on a new line"
   homepage "https://github.com/maahsome/homebrew-utility"
   url "https://github.com/maahsome/homebrew-utility.git"
-  version "0.0.1"
+  version "0.0.2"
 
   def install
     bin.install "bin/showpath"

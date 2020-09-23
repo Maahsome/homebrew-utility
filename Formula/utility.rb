937a2709501ebf90f200979af2583d463be7542b
Christopher Maahs <cmaahs@splicemachine.com>
2020-09-23 00:17:19 -0500

    Initial tool, tn

---
+++ Formula/utility.rb
@@ -0,0 +1,23 @@
+class Utility < Formula
+  desc "This is a collection general utility scripts for Maahsome resources."
+  homepage "https://github.com/maahsome/homebrew-utility"
+  url "https://github.com/maahsome/homebrew-utility.git"
+  version "0.0.1"
+
+  depends_on "fzf"
+  depends_on "mdcat"
+
+  def install
+    bin.install "bin/tn"
+  end
+
+  def caveats; <<~EOS
+    You will need to set MD_NOTEBOOKS_DIR=<path to md-notebooks repository>
+    With that set, you can use 'tn' to quickly access the Markdown notes.
+
+    'tn' alone will display the directories (which represent the cli tools)
+    'tn <tool>' will bring up a list via fzf and allow you to select the 
+                markdown note you want to view.
+  EOS
+  end
+end

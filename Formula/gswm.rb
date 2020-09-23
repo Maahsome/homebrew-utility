202d5796d8d4fb84791cefe1132e85bb51cf4392
Dalton Maahs <dalton.maahs@yourbind.com>
2020-09-23 01:31:57 -0500

    Create gswm: git switch menu.
    A fzf interface for switching between git branches

---
+++ Formula/gswm.rb
@@ -0,0 +1,17 @@
+class Tn < Formula
+  desc "gswm: git switch menu. Uses fzf to switch branches in git"
+  homepage "https://github.com/maahsome/homebrew-utility"
+  url "https://github.com/maahsome/homebrew-utility.git"
+  version "0.0.1"
+
+  depends_on "fzf"
+
+  def install
+    bin.install "bin/gswm"
+  end
+
+  def caveats; <<~EOS
+    Non-Homebrew Dependecies: [sed]
+  EOS
+  end
+end

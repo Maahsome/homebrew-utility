d5373e2e637586bc2d75c8708b0337a44c983580
Dalton Maahs <dalton.maahs@yourbind.com>
2020-10-02 14:18:12 -0500

    filterptalog pipeable in

--- Formula/filterptalog.rb
+++ Formula/filterptalog.rb
@@ -2,7 +2,7 @@ class Filterptalog < Formula
   desc "Filter PTA logs. #PtaTeamRocks"
   homepage "https://github.com/maahsome/homebrew-utility"
   url "https://github.com/maahsome/homebrew-utility.git"
-  version "0.0.2"
+  version "0.0.3"
 
   depends_on "python3"
 

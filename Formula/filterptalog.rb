36cb6cacb3c69ac1703481f316076e312312e5f5
Dalton Maahs <dalton.maahs@yourbind.com>
2020-10-02 13:18:05 -0500

    filterptalog change name

--- Formula/filterptalog.rb
+++ Formula/filterptalog.rb
@@ -2,11 +2,11 @@ class Filterptalog < Formula
   desc "Filter PTA logs. #PtaTeamRocks"
   homepage "https://github.com/maahsome/homebrew-utility"
   url "https://github.com/maahsome/homebrew-utility.git"
-  version "0.0.1"
+  version "0.0.2"
 
   depends_on "python3"
 
   def install
-    bin.install "bin/filter_pta_log"
+    bin.install "bin/filterptalog"
   end
 end
